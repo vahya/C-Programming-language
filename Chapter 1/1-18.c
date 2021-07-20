753ed4e93b1d2478edbd5cc0af3eaae7ef81d8f9
vahya <vahya@protonmail.com>
2021-07-20 16:34:25 +0530

    Fixed 1-18.c

--- Chapter 1/1-18.c
+++ Chapter 1/1-18.c
@@ -8,7 +8,7 @@ lines.
 
 #define MAXLINE 1000 /*maximum input line length*/
 
-int getline(char line[], int maxline);
+int get_line(char line[], int maxline);
 int remove_trail(char s[], int n);
 
 int main()
@@ -16,14 +16,14 @@ int main()
     int len;
     char line[MAXLINE];
 
-    while ((len = remove_trail(line, getline(line, MAXLINE))) > 0)
+    while ((len = remove_trail(line, get_line(line, MAXLINE))) >= 0)
         printf("%d, %s\n", len, line);
 
     return 0;
 }
 
-/*getline: read a line into s, return length*/
-int getline(char s[], int lim)
+/*get_line: read a line into s, return length*/
+int get_line(char s[], int lim)
 {
     int c, i;
 
@@ -47,12 +47,15 @@ int remove_trail(char s[], int n)
     int i;
     for (i = n - 1; i >= 0; --i)
     {
-        if (s[i] != ' ' && s[i] != '\t' && s[i] == '\n')
+        if (s[i] != ' ' && s[i] != '\t' && s[i] != '\n')
         {
             s[++i] = '\0';
             break;
         }
     }
+
+    if (i == -1)
+        s[++i] = '\0';
 
     return i;
 }
